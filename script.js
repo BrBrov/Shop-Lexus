74b6f6d6a94a677a67f28d4fd4a16cfe6c79cad0
BrBrov <epoc@list.ru>
2024-10-20 09:27:18 +0300

    feat: create menu

---
+++ script.js
@@ -0,0 +1,20 @@
+function menuHandler() {
+  const menu = document.querySelector('.header__navigator');
+  menu.classList.add('open-menu');
+
+  const closeMenuBTN = menu.querySelector('.header__close-img');
+  closeMenuBTN.addEventListener('click', () => {
+    menu.classList.remove('open-menu');
+    menu.classList.add('close-menu');
+    setTimeout(() => {menu.classList.remove('close-menu')}, 500);
+  });
+
+}
+
+window.onload = () => {
+  const openMenuBtn = document.querySelector('.header__menu');
+  openMenuBtn.addEventListener('click', menuHandler);
+};
+
+console.log('done!!!');
+
