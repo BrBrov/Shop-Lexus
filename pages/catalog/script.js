003258a5e48814073155d9691e3e72178df05c50
BrBrov <epoc@list.ru>
2024-10-22 11:47:16 +0300

    feat: add multiple range

--- pages/catalog/script.js
+++ pages/catalog/script.js
@@ -1,4 +1,14 @@
+const sliderOne = document.querySelector(".min__range");
+const sliderTwo = document.querySelector(".max__range");
+
+const displayValOne = document.getElementById("price-min-value");
+const displayValTwo = document.getElementById("price-max-value");
+const sliderTrack = document.querySelector(".range__track");
+const minGap = document.querySelector(".min__range").min;
+const sliderMaxValue = document.querySelector(".min__range").max;
+
 window.onload = () => {
+
   $(document).ready(function() {
   $(".faq-question").click(function() {
       $(this).toggleClass("active");
@@ -9,5 +19,3 @@ window.onload = () => {
   });
 });
 };
-
-console.log('done!!!');
