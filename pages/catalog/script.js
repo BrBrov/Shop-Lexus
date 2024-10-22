5efd3fd3935e66ea7fdf2d493df32e55dcb2704b
BrBrov <epoc@list.ru>
2024-10-22 09:53:57 +0300

    fix: fix header mark up

--- pages/catalog/script.js
+++ pages/catalog/script.js
@@ -1,21 +1,3 @@
-// function menuHandler() {
-//   const menu = document.querySelector('.header__navigator');
-//   menu.classList.add('open-menu');
-
-//   const closeMenuBTN = menu.querySelector('.header__close-img');
-//   closeMenuBTN.addEventListener('click', () => {
-//     menu.classList.remove('open-menu');
-//     menu.classList.add('close-menu');
-//     setTimeout(() => {menu.classList.remove('close-menu')}, 500);
-//   });
-
-// }
-
-// window.onload = () => {
-//   const openMenuBtn = document.querySelector('.header__menu');
-//   openMenuBtn.addEventListener('click', menuHandler);
-// };
-
 window.onload = () => {
   $(document).ready(function() {
   $(".faq-question").click(function() {
