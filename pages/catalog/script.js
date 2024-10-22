c52b9912afcdaa042a7f97d6dcd5133e1657ff82
BrBrov <epoc@list.ru>
2024-10-22 12:44:31 +0300

    feat: add logic multiple range

--- pages/catalog/script.js
+++ pages/catalog/script.js
@@ -4,10 +4,26 @@ const sliderTwo = document.querySelector(".max__range");
 const displayValOne = document.getElementById("price-min-value");
 const displayValTwo = document.getElementById("price-max-value");
 const sliderTrack = document.querySelector(".range__track");
-const minGap = document.querySelector(".min__range").min;
+const minGap = 0;
 const sliderMaxValue = document.querySelector(".min__range").max;
 
+function slideOne(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+      sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.textContent = sliderOne.value;
+}
+function slideTwo(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+      sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.textContent = sliderTwo.value;
+}
+
 window.onload = () => {
+
+  sliderOne.addEventListener('input', slideOne);
+  sliderTwo.addEventListener('input', slideTwo);
 
   $(document).ready(function() {
   $(".faq-question").click(function() {
