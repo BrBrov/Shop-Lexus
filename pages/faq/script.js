2576d7ab3cd5bed391ba63b79150124367616ff2
BrBrov <epoc@list.ru>
2024-10-23 13:55:53 +0300

    feat: add faq page

--- pages/faq/script.js
+++ pages/faq/script.js
@@ -1,20 +1,37 @@
-// $(document).ready(function() {
-//     $(".faq-question").click(function() {
-//         $(this).toggleClass("active");
-//         $(this).next(".faq-answer").slideToggle(300);
-//         // Переключение иконки
-//         $(this).find(".toggle-icon");
-//     });
-// });
-
-const faqQuestions = document.querySelectorAll('.faq-item');
-
-faqQuestions.forEach( item => {
-  const question = item.querySelector('.faq-question');
-  const answer = item.querySelector('.faq-answer');
-
-  question.addEventListener('click', () => {
-    question.classList.toggle('active');
-    answer.classList.toggle('open');
-  });
+$(document).ready(function() {
+    $(".faq-question").click(function() {
+        $(this).toggleClass("active");
+        $(this).next(".faq-answer").slideToggle(300);
+        // Переключение иконки
+        $(this).find(".toggle-icon");
+    });
 });
+
+function menuHandler() {
+  const menu = document.querySelector('.header__navigator');
+  menu.classList.add('open-menu');
+
+  const closeMenuBTN = menu.querySelector('.header__close-img');
+  closeMenuBTN.addEventListener('click', () => {
+    menu.classList.remove('open-menu');
+    menu.classList.add('close-menu');
+    setTimeout(() => {menu.classList.remove('close-menu')}, 500);
+  });
+}
+
+window.onload = () => {
+  const openMenuBtn = document.querySelector('.header__menu');
+  openMenuBtn.addEventListener('click', menuHandler);
+};
+
+// const faqQuestions = document.querySelectorAll('.faq-item');
+
+// faqQuestions.forEach( item => {
+//   const question = item.querySelector('.faq-question');
+//   const answer = item.querySelector('.faq-answer');
+
+//   question.addEventListener('click', () => {
+//     question.classList.toggle('active');
+//     answer.classList.toggle('open');
+//   });
+// });
