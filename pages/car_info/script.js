141a7c9315603e0eb2d434272ab5c4d71a2527e9
BrBrov <epoc@list.ru>
2024-10-24 23:36:26 +0300

    feat: add menu script

--- pages/car_info/script.js
+++ pages/car_info/script.js
@@ -8,24 +8,40 @@ function swiperHandler(target) {
   }
 }
 
+function menuHandler() {
+  const menu = document.querySelector('.header__navigator');
+  menu.classList.add('open-menu');
+
+  const closeMenuBTN = menu.querySelector('.header__close-img');
+  closeMenuBTN.addEventListener('click', () => {
+    menu.classList.remove('open-menu');
+    menu.classList.add('close-menu');
+    setTimeout(() => { menu.classList.remove('close-menu') }, 500);
+  });
+}
+
 
 window.onload = () => {
 
   const swiper = new Swiper('.swiper', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
-  mousewheel: {
-    invert: true,
-  }
-});
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+    },
+    mousewheel: {
+      invert: true,
+    }
+  });
 
-swiperHandler({ target: window });
+  swiperHandler({ target: window });
 
-window.addEventListener('resize', swiperHandler);
+  const openMenuBtn = document.querySelector('.header__menu');
+
+  openMenuBtn.addEventListener('click', menuHandler);
+
+  window.addEventListener('resize', swiperHandler);
 }
